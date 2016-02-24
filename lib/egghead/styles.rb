82f2457e0b33caa7332eaf7b36c210ced65ab894
J.S. Leonard <hey@leonardsouza.com>
2016-02-24 09:19:57 -0800

    an attempt to make sass available to egghead-rails

--- lib/egghead/styles.rb
+++ lib/egghead/styles.rb
@@ -4,13 +4,7 @@ module Egghead
   module Styles
 
     class Engine < ::Rails::Engine
-
-      initializer 'egghead-styles.assets.precompile', group: :all do |app|
-        %w(stylesheets).each do |sub|
-          app.config.assets.paths << root.join('assets', sub).to_s
-        end
-      end
-
+      config.assets.paths << Rails.root.join('assets', 'stylesheets').to_s
     end
 
   end
